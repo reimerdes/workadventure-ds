95f32eaf12b02146663a716184ed435bf7abcfae
Christian Kröhnert <christian.kroehnert@dampsoft.de>
2023-12-21 06:45:23 +0100

    Save last position
    * remember the last position on teleport to break area or to the call areas
    * on area leave the last positions will be cleared

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Area } from "@workadventure/iframe-api-typings";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 (async () => {
@@ -5,6 +6,14 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 })();
 
 let tileSize = 32;
+
+interface Position {
+    x: number | undefined;
+    y: number | undefined;
+}
+
+let lastPositionBreak: Position = {x: undefined, y: undefined};
+let lastPositionCall: Position = {x: undefined, y: undefined};
 
 function getRandomInt(min: number, max: number): number {
     return Math.ceil(Math.random() * (max - min + 1)) + min;
@@ -23,7 +32,7 @@ function showOrHideLayer(layerName: string, startDate: Date, endDate: Date) {
     } else {
         WA.room.hideLayer(layerName);
     }
-} 
+}
 
 function showOrHideChristmasLayer() {
     let today = new Date();
@@ -35,6 +44,13 @@ function showOrHideChristmasLayer() {
     showOrHideLayer("Christmas", startDate, endDate);
 }
 
+function clearLastPositions() {
+    lastPositionBreak.x = undefined;
+    lastPositionBreak.y = undefined;
+    lastPositionCall.x = undefined;
+    lastPositionCall.y = undefined;
+}
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     const userTag = WA.player.tags;
@@ -43,22 +59,36 @@ WA.onInit().then(() => {
     if(userTag.includes("admin")) {
         WA.player.setOutlineColor(27, 42, 65);
     }
-    
+
     WA.ui.actionBar.addButton({
         id: 'pause-btn',
         // @ts-ignore
         type: 'action',
         imageSrc: 'https://github.com/othaldo/workadventure-ds/blob/master/src/assets/ds/pause.png?raw=true',
-        toolTip: 'Zum Pausenbereich teleportieren',
+        toolTip: 'Teleport to pause area and back again',
         callback: async () => {
-            const area = await WA.room.area.get("pauseArea");
-            let xStart = area.x;
-            let xEnd = area.x + area.width - (tileSize / 2);
+            let x;
+            let y;
+            if (lastPositionBreak.x === undefined || lastPositionBreak.y === undefined) {
+                const area = await WA.room.area.get("pauseArea");
+                let xStart = area.x;
+                let xEnd = area.x + area.width - (tileSize / 2);
 
-            let yStart = area.y;
-            let yEnd = area.y + area.height - (tileSize / 2);
+                let yStart = area.y;
+                let yEnd = area.y + area.height - (tileSize / 2);
 
-            WA.player.teleport(getRandomInt(xStart , xEnd), getRandomInt(yStart , yEnd));
+                x = getRandomInt(xStart , xEnd);
+                y = getRandomInt(yStart , yEnd);
+                lastPositionBreak.x = (await WA.player.getPosition()).x;
+                lastPositionBreak.y = (await WA.player.getPosition()).y;
+            } else {
+                x = lastPositionBreak.x;
+                y = lastPositionBreak.y;
+                lastPositionBreak.x = undefined;
+                lastPositionBreak.y = undefined;
+            }
+
+            WA.player.teleport(x,y);
         }
     });
 
@@ -69,30 +99,55 @@ WA.onInit().then(() => {
         imageSrc: 'https://github.com/othaldo/workadventure-ds/blob/master/src/assets/ds/call.png?raw=true',
         toolTip: 'Zum \'Im Gespräch\'-Bereich teleportieren',
         callback: async () => {
-            const customerCallArea1 = await WA.room.area.get("ccArea1");
-        const customerCallArea2 = await WA.room.area.get("ccArea2");
-        const position = await WA.player.getPosition();
+            let x;
+            let y;
+            if (lastPositionCall.x === undefined || lastPositionCall.y === undefined) {
+                const customerCallArea1 = await WA.room.area.get("ccArea1");
+                const customerCallArea2 = await WA.room.area.get("ccArea2");
+                const position = await WA.player.getPosition();
 
-        // Berechne die Mittelpunkte der Areas
-        const midPointArea1 = { x: customerCallArea1.x + customerCallArea1.width / 2, y: customerCallArea1.y + customerCallArea1.height / 2 };
-        const midPointArea2 = { x: customerCallArea2.x + customerCallArea2.width / 2, y: customerCallArea2.y + customerCallArea2.height / 2 };
+                // Berechne die Mittelpunkte der Areas
+                const midPointArea1 = { x: customerCallArea1.x + customerCallArea1.width / 2, y: customerCallArea1.y + customerCallArea1.height / 2 };
+                const midPointArea2 = { x: customerCallArea2.x + customerCallArea2.width / 2, y: customerCallArea2.y + customerCallArea2.height / 2 };
 
-        // Berechne die Distanzen zur aktuellen Position
-        const distanceToArea1 = getDistance(position.x, position.y, midPointArea1.x, midPointArea1.y);
-        const distanceToArea2 = getDistance(position.x, position.y, midPointArea2.x, midPointArea2.y);
+                // Berechne die Distanzen zur aktuellen Position
+                const distanceToArea1 = getDistance(position.x, position.y, midPointArea1.x, midPointArea1.y);
+                const distanceToArea2 = getDistance(position.x, position.y, midPointArea2.x, midPointArea2.y);
 
-        // Bestimme die nächstgelegene Area
-        const nearestArea = distanceToArea1 < distanceToArea2 ? customerCallArea1 : customerCallArea2;
+                // Bestimme die nächstgelegene Area
+                const nearestArea = distanceToArea1 < distanceToArea2 ? customerCallArea1 : customerCallArea2;
 
-        // Berechne zufällige Position innerhalb der nächstgelegenen Area
-        let xStart = nearestArea.x;
-        let xEnd = nearestArea.x + nearestArea.width - (tileSize / 2);
-        let yStart = nearestArea.y;
-        let yEnd = nearestArea.y + nearestArea.height - (tileSize / 2);
+                // Berechne zufällige Position innerhalb der nächstgelegenen Area
+                let xStart = nearestArea.x;
+                let xEnd = nearestArea.x + nearestArea.width - (tileSize / 2);
+                let yStart = nearestArea.y;
+                let yEnd = nearestArea.y + nearestArea.height - (tileSize / 2);
 
-        // Teleportiere den Spieler
-        WA.player.teleport(getRandomInt(xStart , xEnd), getRandomInt(yStart , yEnd));
+                x = getRandomInt(xStart , xEnd);
+                y = getRandomInt(yStart , yEnd);
+                lastPositionCall.x = (await WA.player.getPosition()).x;
+                lastPositionCall.y = (await WA.player.getPosition()).y;
+            } else {
+                x = lastPositionCall.x;
+                y = lastPositionCall.y;
+                lastPositionCall.x = undefined;
+                lastPositionCall.y = undefined;
+            }
+
+            WA.player.teleport(x,y);
         }
+    });
+
+    WA.room.area.onLeave("pauseArea").subscribe(() => {
+        clearLastPositions()
+    });
+
+    WA.room.area.onLeave("ccArea1").subscribe(() => {
+        clearLastPositions()
+    });
+
+    WA.room.area.onLeave("ccArea2").subscribe(() => {
+        clearLastPositions()
     });
 
 
