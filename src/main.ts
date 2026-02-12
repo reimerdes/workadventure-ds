22cff8504e948d1f5c8f5b4fb3afca50915ed951
Steven Schwarz <steven.schwarz@dampsoft.de>
2026-02-12 08:08:49 +0100

    - Actions nur anmelden, wenn member tag gesetzt ist - outlineColor angepasst für member und admin

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,40 @@
-import { bootstrapExtra } from '@workadventure/scripting-api-extra';
-import { Holidays } from './holidays.js';
-import { Actions } from './actions.js';
-import { Night } from './night.js';
+import {bootstrapExtra} from '@workadventure/scripting-api-extra';
+
+import {Actions} from './actions.js';
+import {Holidays} from './holidays.js';
+import {Night} from './night.js';
 
 WA.onInit()
-  .then(() => {
-    const userTag = WA.player.tags;
+    .then(() => {
+      const userTag = WA.player.tags;
 
-    // If user is admin, name it with a dark blue border
-    if (userTag.includes('admin')) {
-      WA.player.setOutlineColor(27, 42, 65);
-    }
+      // If user is admin, name it with a dark blue border
+      if (userTag.includes('admin')) {
+        WA.player.setOutlineColor(0xfa, 0xb0, 0x00);
+      }
 
-    Actions.registerActions();
-    Holidays.init();
-    Night.init();
+      if (userTag.includes('member')) {
+        Actions.registerActions();
+        WA.player.setOutlineColor(0x44, 0x99, 0xa9);
+      }
+      Holidays.init();
+      Night.init();
 
-    // Subscriptions sammeln (optional, aber sauberer)
-    WA.room.area.onEnter("gewaechshausArea").subscribe(() => {
-      WA.room.hideLayer('Gewaechshaus');
-    });
+      // Subscriptions sammeln (optional, aber sauberer)
+      WA.room.area.onEnter('gewaechshausArea').subscribe(() => {
+        WA.room.hideLayer('Gewaechshaus');
+      });
 
-    WA.room.area.onLeave("gewaechshausArea").subscribe(() => {
-      WA.room.showLayer('Gewaechshaus');
-    });
+      WA.room.area.onLeave('gewaechshausArea').subscribe(() => {
+        WA.room.showLayer('Gewaechshaus');
+      });
 
-    // The line below bootstraps the Scripting API Extra library that adds a
-    // number of advanced properties/features to WorkAdventure
-    bootstrapExtra()
-      .then(() => {
-        console.log('Scripting API Extra ready');
-      })
-      .catch(e => console.error(e));
-  })
-  .catch(e => console.error(e));
+      // The line below bootstraps the Scripting API Extra library that adds a
+      // number of advanced properties/features to WorkAdventure
+      bootstrapExtra()
+          .then(() => {
+            console.log('Scripting API Extra ready');
+          })
+          .catch(e => console.error(e));
+    })
+    .catch(e => console.error(e));
